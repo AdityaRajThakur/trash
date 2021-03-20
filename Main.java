c7a2bdc6de86cb9ba460bff65a405a08e1848802
AdityaRajThakur <adityaraj715426@gmail.com>
2021-03-20 16:05:55 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/**
+ * secondmain
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println(name);
+        scan.close();
+        System.out.println();
+
+    }
+}
