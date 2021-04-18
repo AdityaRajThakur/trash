333e7b9ec3306080a52cf39d6cc33fc2f56b55f0
AdityaRajThakur <adityaraj715426@gmail.com>
2021-04-17 21:32:19 -0700

    no to changes

--- Main.java
+++ Main.java
@@ -1,103 +1,72 @@
-// fast input output
+/*Author Adityaraj*/
+
 import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		// Initialize the reader
-		FastScanner scan = new FastScanner();
-		// Initialize the writer
-		FastOutput out = new FastOutput();
-/*********************************************************************************************************************************************/
-		// writer your code here
-		while (t-->0) {
-
-		// String str = scan.readString();
-		// int integer = scan.readInt();
-		Long integer = scan.readLong();
-		// integer = solve(integer);
-		// out.writeString(Integer.toBinaryString(integer));
-		// if (solve(integer)) {
-		// 	out.writeString("Yes this integer is a power of two");
-		// }else{
-		// 	out.writeString("NO this number is not a power of two");
-		// }
-
-			
-		int i = solve(integer);
-		out.writeInt(i);
-
-			
-
-
-
-		/*********************************************************************************************************************************************/
-	}	
-		/*********************************************************************************************************************************************/
-		// writer your function code here Accepted
-
-
-// public static Boolean solve(int integer) {
-// 	while ((integer&1)==0) {
-// 		integer>>=1;
-// 	}
-
-// 	if (integer==1) {
-// 		return true;
-// 	}
-// 	return false; 
-// }
-
-// public static int solve(Long integer) {
-// 	int count=0;
-// 	while (integer>0) {
-// 		count++;
-// 		integer&=integer-1;
-// 	}
-// 	return count ;
-// }
-public static int solve(Long integer) {
-	int ans = 1; 
-	while (integer>0) {
-		if ((integer&1)==0) {
-			ans = ans *2;
-		}
-		integer>>=1;
-	}
-	return ans ;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public class Main {
+	public static void main(String[] args) throws IOException {
+	// Initialize the reader
+	FastScanner scan = new FastScanner();
+	// Initialize the writer
+	FastOutput out = new FastOutput();
+	/************************************************************************************************************************************/
+	// writer your code here
+		
+		
 	
+	/*************************************************************************************************************************************/
+	}
+	/**************************************************************************************************************************************/
+	// do not touch it
+	/**************************************************************************************************************************************/
+	//Write here the function which do you want to insert into the code during the sumbition
+	// this function will the gcd of two numbers
+	public static long gcd(long a, long b) {
+		if (b == 0)
+			return a;
+		return gcd(b, a % b);
+	}
 
-/**********************************************************************************************************************************************/
+	// this will return the pow of a^b
+	public static long bin_exp(long a, long b) {
+		long res = 1;
+		while (b != 0) {
+			if (b % 2 != 0)
+				res *= a;
+			a *= a;
+			b /= 2;
+		}
+		return res;
+	}
+
+	// this will return true if a is prime and false if not
+	public static boolean primeornot(long a) {
+		for (int i = 2; i * i <= a; i++) {
+			if (a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// this function will check that a given string is palindrome or not
+	public static boolean palindrome(String string) {
+		StringBuffer buffer = new StringBuffer(string);
+		buffer = buffer.reverse();
+		if (string.equals(buffer.toString())) {
+			return true;
+		}
+		return false;
+	}
+	/*************************************************************************************************************************/
+	/****************************************************************************************************************************************/
 	// Fast Reader Class
 	public static class FastScanner {
 
@@ -105,24 +74,15 @@ public static int solve(Long integer) {
 		BufferedReader reader;
 
 		// Constructor
-		public FastScanner()
-		{
+		public FastScanner() {
 
 			// Initialize the reader
-			reader = new BufferedReader(
-				new InputStreamReader(
-					System.in));
+			reader = new BufferedReader(new InputStreamReader(System.in));
 
-			if (System.getProperty(
-					"ONLINE_JUDGE")
-				== null) {
+			if (System.getProperty("ONLINE_JUDGE") == null) {
 				try {
-					reader = new BufferedReader(
-						new InputStreamReader(
-							new FileInputStream(
-								"input.txt")));
-				}
-				catch (Exception e) {
+					reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+				} catch (Exception e) {
 				}
 			}
 		}
@@ -132,40 +92,55 @@ public static int solve(Long integer) {
 
 		// Function to read a
 		// single integer
-		// to extend the fast reader class writer your function here 
+		// to extend the fast reader class writer your function here
 
-		public int readInt()throws IOException{
-			return Integer.parseInt(
-				reader.readLine());
+		public int readInt() throws IOException {
+			return Integer.parseInt(reader.readLine());
 		}
 
 		// Function to read a
 		// single long
-		public long readLong()throws IOException{
-			return Long.parseLong(
-				reader.readLine());
+		public long readLong() throws IOException {
+			return Long.parseLong(reader.readLine());
 		}
+
 		// Function to read string
-		public String readString()throws IOException{
+		public String readString() throws IOException {
 			return reader.readLine();
 		}
+
 		// Function to read a array
 		// of numsInts integers
 		// in one line
-		public int[] readIntArray(int numInts)throws IOException{
+		public int[] readIntArray(int numInts) throws IOException {
 			int[] nums = new int[numInts];
-			tokenizer
-				= new StringTokenizer(
-					reader.readLine());
+			tokenizer = new StringTokenizer(reader.readLine());
 
-			for (int i = 0;
-				i < numInts; i++) {
-				nums[i] = Integer.parseInt(
-					tokenizer.nextToken());
+			for (int i = 0; i < numInts; i++) {
+				nums[i] = Integer.parseInt(tokenizer.nextToken());
 			}
 			return nums;
 		}
 
+		public Long[] readLongArray(int n) throws IOException {
+			tokenizer = new StringTokenizer(reader.readLine());
+			Long[] arr = new Long[n];
+			int i = 0;
+			while (tokenizer.hasMoreTokens()) {
+				arr[i] = Long.parseLong(tokenizer.nextToken());
+				i++;
+			}
+			return arr;
+		}
+
+		public List<Integer> readIntAsList() throws IOException {
+			List<Integer> list = new ArrayList<Integer>();
+			tokenizer = new StringTokenizer(reader.readLine());
+			while (tokenizer.hasMoreTokens()) {
+				list.add(Integer.parseInt(tokenizer.nextToken()));
+			}
+			return list;
+		}
 
 	}
 
@@ -176,55 +151,45 @@ public static int solve(Long integer) {
 		BufferedWriter writer;
 
 		// Constructor
-		public FastOutput(){
+		public FastOutput() {
 
 			// Initialize the writer
-			writer = new BufferedWriter(
-				new OutputStreamWriter(
-					System.out));
-			if (System.getProperty(
-					"ONLINE_JUDGE")
-				== null) {
+			writer = new BufferedWriter(new OutputStreamWriter(System.out));
+			if (System.getProperty("ONLINE_JUDGE") == null) {
 				try {
-					writer = new BufferedWriter(
-						new OutputStreamWriter(
-							new FileOutputStream(
-								"output.txt")));
-				}
-				catch (Exception e) {
+					writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
+				} catch (Exception e) {
 				}
 			}
 		}
 
 		// Function to write the
 		// single integer
-		// to extend the fast reader class writer your function here 
-		public void writeInt(int i)throws IOException{
+		public void writeInt(int i) throws IOException {
 			writer.write(Integer.toString(i));
 			writer.newLine();
 			writer.flush();
 		}
 
 		// Function to write single long
-		public void writeLong(long i)throws IOException{
+		public void writeLong(long i) throws IOException {
 			writer.write(Long.toString(i));
 			writer.newLine();
 			writer.flush();
 		}
+
 		// Function to write String
-		public void writeString(String s)throws IOException{
+		public void writeString(String s) throws IOException {
 			writer.write(s);
 			writer.newLine();
 			writer.flush();
 		}
-		
+
 		// Function to write a Integer of
 		// array with spaces in one line
-		public void writeIntArray(int[] nums)throws IOException{
-			for (int i = 0;
-				i < nums.length; i++) {
-				writer.write(nums[i]
-							+ " ");
+		public void writeIntArray(int[] nums) throws IOException {
+			for (int i = 0; i < nums.length; i++) {
+				writer.write(nums[i] + " ");
 			}
 			writer.newLine();
 			writer.flush();
@@ -232,20 +197,39 @@ public static int solve(Long integer) {
 
 		// Function to write Integer of
 		// array without spaces in 1 line
-		public void writeIntArrayWithoutSpaces(int[] nums)throws IOException{
-			for (int i = 0;
-				i < nums.length; i++) {
-				writer.write(
-					Integer.toString(
-						nums[i]));
+		public void writeIntArrayWithoutSpaces(int[] nums) throws IOException {
+			for (int i = 0; i < nums.length; i++) {
+				writer.write(Integer.toString(nums[i]));
 			}
 			writer.newLine();
 			writer.flush();
 		}
 
-	}
-	
+		public void writelist(List<Integer> num) throws IOException {
+			if (num != null) {
+				for (Integer integer : num) {
+					writer.write(integer + " ");
 
+				}
+			}
+
+			writer.newLine();
+			writer.flush();
+		}
+
+		public void writer_matrix(int[][] matrix) throws IOException {
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[0].length; j++) {
+					writer.write(matrix[i][j] + " ");
+				}
+				writer.newLine();
+			}
+			writer.newLine();
+			writer.flush();
+
+		}
+
+	}
 
 }
 
